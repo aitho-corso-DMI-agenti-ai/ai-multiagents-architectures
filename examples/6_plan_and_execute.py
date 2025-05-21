c5632e8896693bbf03db1449ad7dd329b328e477
Gabriele Grillo <gabriele.grillo@aitho.it>
2025-05-21 16:08:48 +0200

    feat: minor reworks and documentation

--- examples/6_plan_and_execute.py
+++ examples/6_plan_and_execute.py
@@ -26,9 +26,7 @@ def _():
 def _():
     from langchain_anthropic import ChatAnthropic
     from langgraph.prebuilt import create_react_agent
-    from langgraph.checkpoint.memory import InMemorySaver
     from langgraph.graph import StateGraph, END
-    from utils import print_messages
     from langchain_community.tools import DuckDuckGoSearchResults, DuckDuckGoSearchRun, Tool
     from langchain_community.utilities import DuckDuckGoSearchAPIWrapper
     from typing import List, TypedDict, Tuple, Annotated, Optional
@@ -78,7 +76,70 @@ def _(ChatAnthropic):
 
 @app.cell
 def _(mo):
-    mo.md(r"""## Define the search tool""")
+    mo.md(
+        r"""
+    ## Define the support classes
+    - Plan contains a list of steps. It's the output type of the Planner Agent
+    - PlanExecState contains the state of the system: user request, executed and next steps and the final answer
+    - EvalResult models the reponse of the evaluator agent. it's the output type of the Eval agent
+    """
+    )
+    return
+
+
+@app.cell
+def _(BaseModel, Field, List):
+    class Plan(BaseModel):
+        """Genera un piano step-by-step per l'obiettivo fornito."""
+        steps: List[str] = Field(description="Lista di passi da eseguire")
+    return (Plan,)
+
+
+@app.cell
+def _(Annotated, List, Optional, Tuple, TypedDict, operator):
+    class PlanExecState(TypedDict):
+        input: str
+        plan: List[str]
+        past_steps: Annotated[List[Tuple[str, str]], operator.add]
+        response: str
+        reason: Optional[str]
+    return (PlanExecState,)
+
+
+@app.cell
+def _(BaseModel, Optional):
+    class EvalResult(BaseModel):
+        success: bool
+        reason: Optional[str] = None
+    return (EvalResult,)
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""## Define planner agent""")
+    return
+
+
+@app.cell
+def _(Plan, PlanExecState, model):
+    planner = model.with_structured_output(Plan, method="function_calling")
+
+    def plan_step(state: PlanExecState) -> dict:
+        prompt_input = state["input"]
+        plan_obj : Plan = planner.invoke(prompt_input)
+        return {"plan": plan_obj.steps, "reason": None}
+    return (plan_step,)
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""## Executor""")
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""### Define the search tool""")
     return
 
 
@@ -97,7 +158,7 @@ def _(DuckDuckGoSearchAPIWrapper, DuckDuckGoSearchRun, Tool):
 
 @app.cell
 def _(mo):
-    mo.md(r"""## Define a fake tool to sens emails""")
+    mo.md(r"""### Define a fake tool to sens emails""")
     return
 
 
@@ -123,51 +184,21 @@ def _(tool):
 
 @app.cell
 def _(mo):
-    mo.md(r"""## Define the support classes""")
+    mo.md(r"""### Define executor agent""")
     return
 
 
 @app.cell
-def _(BaseModel, Field, List):
-    class Plan(BaseModel):
-        """Genera un piano step-by-step per l'obiettivo fornito."""
-        steps: List[str] = Field(description="Lista di passi da eseguire")
-    return (Plan,)
+def _(PlanExecState, create_react_agent, model, search_tool, send_email):
+    executor_agent = create_react_agent(
+        model=model,
+        tools=[search_tool, send_email],
+        prompt="Sei un esecutore di task con accesso a un tool per effettuare una ricerca sul web e uno per inviare una mail. Se non sai quale sia il destinatario non inviare la mail, limitati a rispondere con il body e il subject",
+        name="executor",
+        debug=True,
+    )
 
-
-@app.cell
-def _(BaseModel, Optional):
-    class EvalResult(BaseModel):
-        success: bool
-        reason: Optional[str] = None
-    return (EvalResult,)
-
-
-@app.cell
-def _(Annotated, List, Optional, Tuple, TypedDict, operator):
-    class PlanExecState(TypedDict):
-        input: str
-        plan: List[str]
-        past_steps: Annotated[List[Tuple[str, str]], operator.add]
-        response: str
-        reason: Optional[str]
-    return (PlanExecState,)
-
-
-@app.cell
-def _(mo):
-    mo.md(r"""## Define planner agent""")
-    return
-
-
-@app.cell
-def _(EvalResult, PlanExecState, model):
-    def plan_step(state: PlanExecState, planner) -> dict:
-        prompt_input = state["input"]
-        plan_obj = planner.invoke(prompt_input)
-        return {"plan": plan_obj.steps, "reason": None}
-
-    def exec_step(state: PlanExecState, executor_agent) -> dict:
+    def exec_step(state: PlanExecState) -> dict:
         task = state["plan"].pop(0)
         history = "\n".join([f"{i+1}. {t} -> {o}" for i, (t, o) in enumerate(state["past_steps"])])
         plan_text = history + f"\n{len(state['past_steps'])+1}. {task} -> TODO"
@@ -177,83 +208,128 @@ def _(EvalResult, PlanExecState, model):
     Tienine conto solo per contesto e per sapere i risultati delle operazioni precedenti, ma della pianificazione dei prossimi task si occupa un agente dedicato, non tu. 
     Non comportarti come un chatbot, ma come un esecutore di task, riporta i risultati in maniera completa ma breve, non aggiungere domande chiedendo se serve ulteriore aiuto.
     Non tornare mai un messaggio vuoto altrimenti verrai valutato negativamente, indica il risultato del task eseguito
+    Stai attento alla differenza fra PREPARARE e INVIARE una mail. Nel primo caso non devi inviarla, solo scriverne il contenuto
 
     Il tuo compito è di eseguire ESCLUSIVAMENTE con 1 tool e ESCLUSIVAMENTE questo task: '{task}'
     """.format(plan_tasks=plan_text, task=task)
         res = executor_agent.invoke({"messages": [{"role": "user", "content": prompt}]})
         ans = res["messages"][-1].content
         return {"past_steps": [(task, ans)]}
+    return exec_step, executor_agent
 
+
+@app.cell
+def _(mo):
+    mo.md(r"""## Evaluator""")
+    return
+
+
+@app.cell
+def _(EvalResult, PlanExecState, model):
     def evaluate_task_step(state: PlanExecState) -> dict:
         last_task, last_output = state["past_steps"][-1]
         evaluator = model.with_structured_output(EvalResult, method="function_calling")
         prompt = f"Hai eseguito il task: '{last_task}' con risultato: '{last_output}'. Valuta se il task è andato a buon fine e, in caso negativo, fornisci la reason. Non valutare mai la correttezza delle date"
-        eval_result = evaluator.invoke(prompt)
+        eval_result : EvalResult = evaluator.invoke(prompt)
 
         return {
             "success": eval_result.success,
             "reason": eval_result.reason,
             "plan": state["plan"],
         }
+    return (evaluate_task_step,)
 
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+    ## Finalize
+
+    """
+    )
+    return
+
+
+@app.cell
+def _(PlanExecState):
     def finalize(state: PlanExecState) -> dict:
-
-        
         lines = [f"{i+1}. {task}: {output}" for i, (task, output) in enumerate(state["past_steps"])]
         final = "\n".join(lines)
-        if state["reason"]:
-            final = "Plan failed: " + state["reason"] + final
         return {"response": final}
 
-    return evaluate_task_step, exec_step, finalize, plan_step
+    return (finalize,)
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""## Build the graph""")
+    return
 
 
 @app.cell
 def _(
     END,
-    Plan,
     PlanExecState,
     StateGraph,
     evaluate_task_step,
     exec_step,
     finalize,
-    model,
     plan_step,
 ):
-    def build_graph(executor_agent, max_failures=3):
-        planner = model.with_structured_output(Plan, method="function_calling")
-        wf = StateGraph(PlanExecState)
+    wf = StateGraph(PlanExecState)
 
-        wf.add_node("planner", lambda s: plan_step(s, planner))
-        wf.add_node("executor", lambda s: exec_step(s, executor_agent))
-        wf.add_node("task_evaluator", evaluate_task_step)
-        wf.add_node("finalize", finalize)
+    wf.add_node("planner", lambda s: plan_step(s))
+    wf.add_node("executor", lambda s: exec_step(s))
+    wf.add_node("task_evaluator", evaluate_task_step)
+    wf.add_node("finalize", finalize)
 
-        wf.set_entry_point("planner")
-        wf.add_edge("planner", "executor")
-        wf.add_edge("executor", "task_evaluator")
+    wf.set_entry_point("planner")
+    wf.add_edge("planner", "executor")
+    wf.add_edge("executor", "task_evaluator")
 
-        wf.add_conditional_edges(
-            "task_evaluator",
-            lambda s: (
-                "finalize" if s.get("success") and not s.get("plan")
-                else ("executor" if s.get("success")
-                      else "finalize")
-            ),
-            {"executor": "executor", "planner": "planner", "finalize": "finalize", END: END}
-        )
+    wf.add_conditional_edges(
+        "task_evaluator",
+        lambda s: (
+            "executor" if s.get("success") and s.get("plan") else "finalize"
+        ),
+        {"executor": "executor", "finalize": "finalize"}
+    )
 
-        wf.add_edge("finalize", END)
+    wf.add_edge("finalize", END)
 
-        return wf.compile()
+    app = wf.compile()
 
-    return (build_graph,)
+    return (app,)
 
 
 @app.cell
-def _(List, Optional, PlanExecState, Tool, build_graph):
+def _(mo):
+    mo.md(r"""##Visualize the graph""")
+    return
+
+
+@app.cell
+def _(app, mo):
+    mo.mermaid(app.get_graph().draw_mermaid())
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+    ## Last touches
+    Let's define an helper function to invoke the planner and execute workflow
+    """
+    )
+    return
+
+
+@app.cell
+def _(List, Optional, PlanExecState, Tool, app):
     def run_agent(user_query: str, executor_agent, tools: Optional[List[Tool]] = None):
-        agents_prompt = ""
+        tools_prompt = ""
         if tools:
             tools_prompt = (
                 "Il tuo executor può usare i seguenti agenti:\n"
@@ -281,21 +357,14 @@ def _(List, Optional, PlanExecState, Tool, build_graph):
             "failure_count": 0,
         }
 
-        app = build_graph(executor_agent)
         return app.invoke(initial_state)
 
     return (run_agent,)
 
 
 @app.cell
-def _(create_react_agent, model, run_agent, search_tool, send_email):
-    executor_agent = create_react_agent(
-        model=model,
-        tools=[search_tool, send_email],
-        prompt="Sei un esecutore di task con accesso a un tool per effettuare una ricerca sul web e uno per inviare una mail. Se non sai quale sia il destinatario non inviare la mail, limitati a rispondere con il body e il subject",
-        name="executor",
-        debug=True,
-    )
+def _(executor_agent, run_agent, search_tool, send_email):
+
 
     query = "Invia all'indirizzo email paperino@topolin.ia la popolazione della città con il grattacielo più alto del mondo"
     result = run_agent(query, executor_agent, tools=[search_tool, send_email])
@@ -304,7 +373,7 @@ def _(create_react_agent, model, run_agent, search_tool, send_email):
 
 @app.cell
 def _(result):
-    print(result["response"])
+    print(result['response'])
     return
 
 
